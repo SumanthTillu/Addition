88c6e6973becab9b1e6df44964ad9745b95cadc4
Sumanth <challasumanth0815@gmail.com>
2021-12-16 12:35:58 +0530

    first commit

---
+++ Addition.java
@@ -0,0 +1,13 @@
+class Addition
+{
+	public static void add(double d , int a)
+	{ 
+		System.out.println(d+a);
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("exe starts ......");
+		add(2.2,3);
+		System.out.println("exe ends ......");
+	}
+}
